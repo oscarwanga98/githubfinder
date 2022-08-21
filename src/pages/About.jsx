62483c509de3cacd4da58141aec568e37b6d1acb
oscarwanga98 <87162511+oscarwanga98@users.noreply.github.com>
2022-08-21 19:04:09 +0300

    1st commit
    sending finder to github

---
+++ src/pages/About.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function About() {
+  return (
+    <>
+    
+        <h1 className="tetx-6xl mb-4">Gihubfinder</h1>
+        <p className='mb-4 text-2xl font-light'>
+          A React app to search GitHub profiles and see profile details. This
+          project is part of my learning exercise to master React frontend development using Tailwind CSS.
+        </p>
+        <p className='text-lg text-gray-400'>
+          Version <span className='text-white'>1.0.0</span>
+        </p>
+     </>
+  )
+}
+
+export default About
